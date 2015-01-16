9071e02720b755ecce6aefd21b154ff7a2e77525
unknown <rookfighter@outlook.de>
2015-01-16 14:19:28 +0100

    minor fixes

--- accumulator.vhd
+++ accumulator.vhd
@@ -1,25 +1,24 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_arith.ALL;
-USE ieee.std_logic_signed.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY accumulator IS
     GENERIC(N: natural := 8;
             N_in: natural := 8;
             RSTDEF: std_logic := '0');
-    PORT(rst:     IN std_logic;                           -- reset, RSTDEF active
-         clk:     IN std_logic;                           -- clock, rising edge
-         swrst:   IN std_logic;                           -- software reset, RSTDEF active
-         en:      IN std_logic;                           -- enable, high active
-         op:      IN std_logic_vector(N_in-1 DOWNTO 0);   -- operand
-         sum:     OUT std_logic_vector(N-1 DOWNTO 0));    -- result
+    PORT(rst:   IN std_logic;                           -- reset, RSTDEF active
+         clk:   IN std_logic;                           -- clock, rising edge
+         swrst: IN std_logic;                           -- software reset, RSTDEF active
+         en:    IN std_logic;                           -- enable, high active
+         op:    IN std_logic_vector(N_in-1 DOWNTO 0);   -- operand
+         sum:   OUT std_logic_vector(N-1 DOWNTO 0));    -- result
 END accumulator;
 
 ARCHITECTURE behavioral OF accumulator IS
-    SIGNAL tmp_sum: std_logic_vector(N-1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL tmp_sum: signed(N-1 DOWNTO 0) := (OTHERS => '0');
 BEGIN
 
-    sum <= tmp_sum;
+    sum <= std_logic_vector(tmp_sum);
         
     PROCESS(rst, clk)
     BEGIN
@@ -29,8 +28,10 @@ BEGIN
             IF swrst = RSTDEF THEN
                 tmp_sum <= (OTHERS => '0');
             ELSIF en = '1' THEN
-                tmp_sum <= tmp_sum + op;
+                -- only apply new value when enabled
+                tmp_sum <= tmp_sum + signed(op);
             END IF;
         END IF;
     END PROCESS;
+
 END behavioral;
