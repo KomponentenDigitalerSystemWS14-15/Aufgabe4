72e9ab561f0a4823532ffe76d06b27ddd181e37a
JeGa <rookfighter@outlook.de>
2015-01-16 16:32:26 +0100

    omptimized addr gen

--- addr_gen.vhd
+++ addr_gen.vhd
@@ -13,47 +13,20 @@ ENTITY addr_gen IS
 END addr_gen;
 
 ARCHITECTURE behavioral OF addr_gen IS
-    SIGNAL rowa: unsigned(3 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL cola: unsigned(3 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL rowb: unsigned(3 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL colb: unsigned(3 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL matr_addr: unsigned(11 DOWNTO 0) := (OTHERS => '0');
 BEGIN
-    addra <= std_logic_vector("00" & rowa & cola);
-    addrb <= std_logic_vector("01" & rowb & colb);
+    addra <= std_logic_vector("00" & matr_addr(11 DOWNTO 8) & matr_addr(3 DOWNTO 0));
+    addrb <= std_logic_vector("01" & matr_addr(3 DOWNTO 0) & matr_addr(7 DOWNTO 4));
 
     PROCESS(rst, clk)
-        VARIABLE rowb_var: unsigned(4 DOWNTO 0) := (OTHERS => '0');
-        VARIABLE colb_var: unsigned(4 DOWNTO 0) := (OTHERS => '0');
     BEGIN
         IF rst = RSTDEF THEN
-            rowa <= (OTHERS => '0');
-            cola <= (OTHERS => '0');
-            rowb <= (OTHERS => '0');
-            colb <= (OTHERS => '0');
-            rowb_var := (OTHERS => '0');
-            colb_var := (OTHERS => '0');
+            matr_addr <= (OTHERS => '0');
         ELSIF rising_edge(clk) THEN
             IF swrst = RSTDEF THEN
-                rowa <= (OTHERS => '0');
-                cola <= (OTHERS => '0');
-                rowb <= (OTHERS => '0');
-                colb <= (OTHERS => '0');
-                rowb_var := (OTHERS => '0');
-                colb_var := (OTHERS => '0');
+                matr_addr <= (OTHERS => '0');
             ELSIF en = '1' THEN
-                
-                rowb_var := ('0' & rowb) + 1;
-                rowb <= rowb_var(3 DOWNTO 0);
-                cola <= cola + 1;
-                
-                IF rowb_var(4) = '1' THEN
-                    colb_var := ('0' & colb) + 1;
-                    colb <= colb_var(3 DOWNTO 0);
-                    
-                    IF colb_var(4) = '1' THEN
-                        rowa <= rowa + 1;
-                    END IF;
-                END IF;
+               matr_addr <= matr_addr + 1;
             END IF;
         END IF;
     END PROCESS;
