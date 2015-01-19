ace2eb3ed1b1253c5e273acd83835b548540cab1
JeGa <jega@win>
2015-01-19 10:35:28 +0100

    test

--- core.vhd
+++ core.vhd
@@ -3,8 +3,8 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY core IS
-   GENERIC(RSTDEF: std_logic := '0');
-   PORT(rst:   IN  std_logic;                      -- reset,          RSTDEF active
+    GENERIC(RSTDEF: std_logic := '0');
+    PORT(rst:   IN  std_logic;                      -- reset,          RSTDEF active
         clk:   IN  std_logic;                      -- clock,          rising edge
         swrst: IN  std_logic;                      -- software reset, RSTDEF active
         -- handshake signals
@@ -72,152 +72,163 @@ ARCHITECTURE behavioral OF core IS
           wea:   IN  std_logic);
     END COMPONENT;
     
-    TYPE TState IS (S0, S1, S2, S3);
+	TYPE TState IS (STOP, S0, S1, S2, S3, S4, S5, S6);
     CONSTANT ACC_LEN: natural := 44;
     CONSTANT ACC_IN_LEN: natural := 36;
     
-    SIGNAL addra: std_logic_vector(9 DOWNTO 0);
-    SIGNAL addrb: std_logic_vector(9 DOWNTO 0);
-    SIGNAL addrc: std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL addrd: std_logic_vector(9 DOWNTO 0);
-    SIGNAL en_store: std_logic_vector(2 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL next_store: std_logic_vector(2 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL state: TState := S0;
+	SIGNAL status: TState := STOP;
+	
+	-- ROM
+    SIGNAL addra: std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL addrb: std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+	
+	-- RAM
+	SIGNAL addrc: std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL addrd: std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+	
+	-- Enables
+	SIGNAL en_rom: std_logic := '0';
+	SIGNAL en_mul: std_logic := '0';
+	SIGNAL en_acc: std_logic := '0';
+	SIGNAL en_ram: std_logic := '0';
+	SIGNAL en_addr_gen: std_logic := '0';
     
+	-- Resets
+	SIGNAL swrst_acc: std_logic := '0';
+	
     SIGNAL vala: std_logic_vector(15 DOWNTO 0) := (OTHERS => '0');
     SIGNAL valb: std_logic_vector(15 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL prod: std_logic_vector(35 DOWNTO 0);
-    SIGNAL sum: std_logic_vector(43 DOWNTO 0);
-    SIGNAL sum_save: std_logic_vector(15 DOWNTO 0);
+	
+	SIGNAL running: std_logic := '0';
     
-    SIGNAL swrst_done: std_logic := NOT RSTDEF;
-    SIGNAL swrst_res: std_logic;
-    SIGNAL swrst_acc_tmp: std_logic := NOT RSTDEF;
-    SIGNAL swrst_acc: std_logic;
 BEGIN
     
-    swrst_res <= swrst WHEN swrst = RSTDEF ELSE swrst_done;
-    swrst_acc <= swrst_res WHEN swrst_res = RSTDEF ELSE swrst_acc_tmp;
-    sum_save <= std_logic_vector(resize(signed(sum), 16));
-    addrd <= "00" & sw;
-    
+	en_rom <= running;
+	en_addr_gen <= running;
+	
     PROCESS(rst, clk)
     BEGIN
         IF rst = RSTDEF THEN
-            en_store <= (OTHERS => '0');
-            next_store <= (OTHERS => '0');
-            state <= S0;
-            swrst_done <= NOT RSTDEF;
-            addrc <= (OTHERS => '0');
+            
         ELSIF rising_edge(clk) THEN
             IF swrst = RSTDEF THEN
-                en_store <= (OTHERS => '0');
-                next_store <= (OTHERS => '0');
-                state <= S0;
-                swrst_done <= NOT RSTDEF;
-                addrc <= (OTHERS => '0');
+                
             ELSE
-                CASE state IS
-                WHEN S0 =>
-                    IF strt = '1' THEN
-                        state <= S1;
-                        en_store(0) <= '1';    
-                        rdy <= '0';
-                    END IF;
-                WHEN S1 =>
-                    -- shift enable signals
-                    en_store <= en_store(en_store'high-1 DOWNTO 0) & '1';
-                    IF en_store(en_store'high-1) = '1' THEN
-                        -- we enabled last component in this clock
-                        state <= S2;
-                    END IF;
-                WHEN S2 =>
-                    en_store(0) <= '1';
-                     -- shift next signals
-                    next_store <= next_store(next_store'high-1 DOWNTO 0) & '0';
-                    IF addra(3 DOWNTO 0) = "1111" THEN
-                        -- finished one dot product
-                        next_store(0) <= '1';
-                        -- interleave 1 clock for accumulator reset
-                        en_store(0) <= '0';
-                    END IF;
-                    
-                    swrst_acc_tmp <= NOT RSTDEF;
-                    IF next_store(1) = '1' THEN
-                        -- reset accumulator
-                        swrst_acc_tmp <= RSTDEF;
-                    END IF;
-                    
-                    IF next_store(2) = '1' THEN
-                        next_store <= (OTHERS => '0');
-                        addrc <= std_logic_vector(unsigned(addrc) + 1);
-                        
-                        -- finished all 
-                        IF addrc = "0011111111" THEN
-                            state <= S3;
-                            en_store <= (OTHERS => '0');
-                            addrc <= (OTHERS => '0');
-                            swrst_done <= RSTDEF;
-                        END IF;
-                    END IF;
-                WHEN S3 =>
-                    state <= S0;
-                    rdy <= '1';
-                    swrst_done <= NOT RSTDEF;
-                END CASE;
+			    -- Start
+				IF strt = '1' THEN
+					running <= '1';
+					rdy <= '0';
+					status <= S0;
+				END IF;
+				
+				IF status = S0 THEN
+					en_mul <= '1';
+					status <= S1;
+				END IF;
+				
+				IF status = S1 THEN
+					en_acc <= '1';
+					status <= S2;
+				END IF;
+				
+				-- Full pipe
+				IF status = S2 THEN			
+				    -- Reset accumulator + write to RAM
+					-- signal at acc in is new sum, signal at acc out gets written
+					IF addra(3 DOWNTO 0) = "0001" THEN -- ACHTUNG HIER
+						swrst_acc <= '1';
+						en_ram <= '1';
+					ELSE
+						swrst_acc <= '0';
+						en_ram <= '0';
+					END IF;
+					
+					-- Finished
+					IF addra = "1111111111" THEN
+						 running <= '0'; -- Stop addr_gen and ROM
+						 status <= S3;
+					END IF;
+				END IF;
+				
+				-- End
+				IF status = S3 THEN
+					en_mul <= '0';
+					status <= S4;
+				END IF;
+				
+				IF status = S4 THEN
+					en_mul <= '0';
+					status <= S5;
+				END IF;
+				
+				IF status = S5 THEN
+					en_acc <= '0';
+					swrst_acc <= '1';
+					
+					en_ram <= '1';
+					status <= S5;
+				END IF;
+				
+				IF status = S6 THEN
+					swrst_acc <= '0';
+					en_ram <= '0';
+				
+					running <= '0';
+					rdy <= '1';
+					status <= STOP;
+				END IF;
             END IF;
         END IF;
     END PROCESS;
 
-    -- addrgen and rom must be enabled in same clock
     addr1: addr_gen
     GENERIC MAP(RSTDEF => RSTDEF)
     PORT MAP(rst => rst,
              clk => clk,
-             swrst => swrst_res,
-             en => en_store(0),
+             swrst => swrst, -- todo
+             en => en_addr_gen,
              addra => addra,
              addrb => addrb);
     
-    rb1: rom_block
-    PORT MAP(addra => addra,
-             addrb => addrb,
-             clka => clk,
-             clkb => clk,
-             douta => vala,
-             doutb => valb,
-             ena => en_store(0),
-             enb => en_store(0));
+ --  rb1: rom_block
+ --  PORT MAP(addra => addra,
+ --           addrb => addrb,
+ --           clka => clk,
+ --           clkb => clk,
+ --           douta => vala,
+ --           doutb => valb,
+ --           ena => en_rom,
+ --           enb => en_rom);
              
-    mul1: multiplier_16x16
-    PORT MAP(clk => clk,
-             clken => en_store(1),
-             swrst => swrst_res,
-             op1 => vala,
-             op2 => valb,
-             prod => prod);
-             
-    acc1: accumulator
-    GENERIC MAP(N => ACC_LEN,
-                N_in => ACC_IN_LEN,
-                RSTDEF => RSTDEF)
-    PORT MAP(rst => rst,
-             clk => clk,
-             swrst => swrst_acc,
-             en => en_store(2),
-             op => prod,
-             sum => sum);
-    
-    rb2: ram_block
-    PORT MAP(addra => addrc,
-             addrb => addrd,
-             clka => clk,
-             clkb => clk,
-             dina => sum_save,
-             douta => OPEN,
-             doutb => dout,
-             ena => '1',
-             enb => '1',
-             wea => next_store(2));
+ --  mul1: multiplier_16x16
+ --  PORT MAP(clk => clk,
+ --           clken => en_store(1),
+ --           swrst => swrst_res,
+ --           op1 => vala,
+ --           op2 => valb,
+ --           prod => prod);
+ --           
+ --  acc1: accumulator
+ --  GENERIC MAP(N => ACC_LEN,
+ --              N_in => ACC_IN_LEN,
+ --              RSTDEF => RSTDEF)
+ --  PORT MAP(rst => rst,
+ --           clk => clk,
+ --           swrst => swrst_acc,
+ --           en => en_store(2),
+ --           op => prod,
+ --           sum => sum);
+ --  
+ --  rb2: ram_block
+ --  PORT MAP(addra => addrc,
+ --           addrb => addrd,
+ --           clka => clk,
+ --           clkb => clk,
+ --           dina => sum_save,
+ --           douta => OPEN,
+ --           doutb => dout,
+ --           ena => '1',
+ --           enb => '1',
+ --           wea => next_store(2));
              
 END behavioral;
